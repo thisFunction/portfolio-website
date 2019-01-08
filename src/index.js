a7bd8abe54b3b8e63030ac77735124680cc73003
Adam Skoczylas <skocadam@gmail.com>
2019-01-08 18:20:55 -0500

    gitpages fixes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {Router , Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
+
 import './css-reset.css';
 import './index.scss';
+
 import MainPage from './components/home-page/home-page'
 import PlaygroundPage from './components/playground-page/playground-page'
 import ContactPage from './components/contact-page/contact-page'
-
 import ErrorPage from './components/error-page/error-page'
-import createHashHistory from 'history/createHashHistory';
-
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 render(
-    <Router history={hashHistory}>
+    <Router basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path="/" component={MainPage} exact />
             <Route path="/playground" component={PlaygroundPage} />
-
             <Route path="/contact" component={ContactPage} />
             <Route component={ErrorPage} />
         </Switch>
