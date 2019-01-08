dfcb060ee87228f3b9f15b2ed7b9795fe1edd3ea
Adam Skoczylas <skocadam@gmail.com>
2019-01-08 15:57:03 -0500

    modify index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ import PlaygroundPage from './components/playground-page/playground-page'
 import ContactPage from './components/contact-page/contact-page'
 
 import ErrorPage from './components/error-page/error-page'
+import createHashHistory from 'history/createHashHistory';
+
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 render(
-    <BrowserRouter>
+    <BrowserRouter history={hashHistory}>
         <Switch>
             <Route path="/" component={MainPage} exact />
             <Route path="/playground" component={PlaygroundPage} />
