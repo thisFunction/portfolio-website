e2dacb0300bca5453f8060a5370393c98c6371ab
Adam Skoczylas <skocadam@gmail.com>
2019-01-23 14:54:13 -0500

    add schemas for mongo db

--- models/dojo.js
+++ models/dojo.js
@@ -6,12 +6,7 @@ const DojoSchema = new Schema({
     name: {
         type: String,
         required: true
-    },
-    dob: {
-        type: String
     }
 });
 
-const Dojo = mongoose.model('Dojo', DojoSchema);
-
-module.exports = Dojo;
+module.exports = Dojo = mongoose.model('Dojo', DojoSchema);
--- models/profile.js
+++ models/profile.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create schema
-const PostsSchema = new Schema({
-    name: {
+const ProfileSchema = new Schema({
+    title: {
         type: String,
         required: true
     },
-    dob: {
-        type: String
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
     }
 });
 
