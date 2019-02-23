3261150f7d6bcac4f4c709fedf53d71e4edc6e07
Adam Skoczylas <skocadam@gmail.com>
2019-02-22 21:00:14 -0500

    display form errors

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
+import classnames from 'classnames';
 import axios from 'axios'
+
 class Register extends Component {
 	constructor() {
 		super();
@@ -16,7 +18,6 @@ class Register extends Component {
     }
     onSubmit=(e) =>{
         e.preventDefault();
-
         const newUser = {
             name: this.state.name,
             email: this.state.email,
@@ -26,38 +27,53 @@ class Register extends Component {
         .then(result => {
             console.log(result.data)
         })
-        .catch(error => console.log(error))
+        .catch(error => this.setState({errors: error.response.data}))
     }
 	render() {
+        const { errors } = this.state;
+
 		return (
 			<div>
-				<form onSubmit={this.onSubmit}>
+				<form noValidate onSubmit={this.onSubmit}>
                     <label>Name</label>
                     <input 
                         type="text"
                         name="name"
+                        className={classnames('',{
+                            'is-invalid': errors.name
+                        })}
                         value={this.state.name}
                         onChange={this.onChange}
                         autoComplete="name"
                     />
+                    {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
 					<br />
 					<label>Email</label>
                     <input
                         type="text"
                         name="email"
+                        className={classnames('',{
+                            'is-invalid': errors.email
+                        })}
                         value={this.state.email}
                         onChange={this.onChange}
                         autoComplete="email"
                     />
+                    {errors.name && (<div className="invalid-feedback">{errors.email}</div>)}
 					<br />
 					<label>Password</label>
 					<input
                         type="password"
                         name="password"
+                        className={classnames('',{
+                            'is-invalid': errors.password
+                        })}
                         value={this.state.password}
                         onChange={this.onChange}
                         autoComplete="new-password"
-                    /> <br/> 
+                    />
+                    {errors.name && (<div className="invalid-feedback">{errors.password}</div>)}
+                    <br/> 
                     <input type="submit"/>
 				</form>
 			</div>
